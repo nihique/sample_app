50a2a15f0f90b9048ea62df0b862e693a63d0c50
Martin Kovarik <kovarik.martin@gmail.com>
2012-03-05 10:06:07 +0100

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,13 @@ describe "Static pages" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | Home")
     end
 
   end
@@ -15,7 +21,13 @@ describe "Static pages" do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | Help")
     end
 
   end
@@ -24,7 +36,13 @@ describe "Static pages" do
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
 
   end
