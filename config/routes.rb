9ade907f51ba62a7b6fd9bcd6558c0b772ddc200
Martin Kovarik <kovarik.martin@gmail.com>
2012-03-20 22:45:44 +0100

    signup page implementation and specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  get 'users/new'
 
   root :to => 'static_pages#home'
+
+  match '/signup', :to => 'users#new'
 
   match '/help', :to => 'static_pages#help'
   match '/about', :to => 'static_pages#about'
