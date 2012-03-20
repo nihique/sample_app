583bcdece45b7869065c2fbc0cfa8d82f3be6adf
Martin Kovarik <kovarik.martin@gmail.com>
2012-03-20 23:07:13 +0100

    excercise 5.6.2 - tests for visiting links on root_path

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,8 +5,8 @@ describe "Static pages" do
   subject { page }
 
   shared_examples_for 'all static pages' do
-    it { should have_selector('h1', text: heading) }
-    it { should have_selector('title', text: full_title(page_title)) }
+    it { should have_selector 'h1',    text: heading  }
+    it { should have_selector 'title', text: full_title(page_title)  }
   end
 
   describe "Home page" do
@@ -15,7 +15,7 @@ describe "Static pages" do
     let(:page_title) { ''}
 
     it_should_behave_like 'all static pages'
-    it { should_not have_selector('title', text: "| Home") }
+    it { should_not have_selector 'title', text: "| Home"  }
   end
 
   describe "Help page" do
@@ -40,6 +40,20 @@ describe "Static pages" do
     let(:page_title) { 'Contact'}
 
     it_should_behave_like 'all static pages'
+  end
+
+  it 'should have the right links on the layout' do
+    visit root_path
+    click_link 'About'
+    page.should have_selector 'title', text: full_title('About')
+    click_link 'Help'
+    page.should have_selector 'title', text: full_title('Help')
+    click_link 'Contact'
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link 'Home'
+    page.should have_selector 'title', text: full_title('')
+    click_link 'Sign up now!'
+    page.should have_selector 'title', text: full_title('Sign up')
   end
 
 end
