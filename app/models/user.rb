f0920bda93fe1b42634b82384817296181636d4e
Martin Kovarik <kovarik.martin@gmail.com>
2012-03-28 01:22:40 +0200

    sublime text 2 project files

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
   validates :name,  presence: true, length: { maximum: 50 }
 
   validates :email, presence: true,
-                    format: { with: VALID_EMAIL_REGEX },
+                    format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: { case_sensitive: false }
 
   validates :password, length: { minimum: 6 }
